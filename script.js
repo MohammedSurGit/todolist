4dad9ce99923cbcd704157c6b9e1ac6755380539
MohammedSurGit <mohalementeur@gmail.com>
2025-01-31 21:31:57 +0100

    Ajout de textInfos et correction de bugs

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 const btnAjouter = document.querySelector('#ajouter');
 const listTache = document.querySelector('#listeTache');
 const toutSupprBtn = document.querySelectorAll('.btnToutSuppr');
+const textInfos = document.querySelector('#textInfos');
 
-btnAjouter.addEventListener('click', () => {
-    /* Récupere la valeur de l'input */
+function evenement() {
     const valeur = document.querySelector('#tache').value;
     /* console.log(nouvelleTache); */
+
+    const VerifInput = valeur.trim();
+
+    if (VerifInput === "") {
+        textInfos.textContent = "Veuillez remplir le champs de texte";
+        textInfos.classList.add('textInfosError');
+        preventDefault();
+    } 
 
     const newDiv =  document.createElement('div');
 
@@ -33,6 +41,23 @@ btnAjouter.addEventListener('click', () => {
 
     listTache.appendChild(newDiv);
 
+
+    if (listTache.contains(newDiv)) {
+        textInfos.textContent = "La tâche a été ajouté avec succès";
+        textInfos.classList.remove('textInfosError');
+        setTimeout(() => {
+            textInfos.textContent = "";
+            setTimeout( () => {
+                let nbEnfants = listTache.childNodes.length;
+                if (!textInfos.textContent.includes(`Vous avez ${nbEnfants - 1}`)) {
+                    if ( nbEnfants < 3 ) {
+                        textInfos.textContent = `Vous avez ${nbEnfants - 1} tâche en cours`;
+                    } else {textInfos.textContent = `Vous avez ${nbEnfants - 1} tâches en cours`};
+                }
+            })
+        }, 2500);
+    }
+
     /* Ajout un text-decoration au click de newDiv */
     newDiv.addEventListener('click', () => {
         const textDecoration = window.getComputedStyle(nouvelleTache).textDecoration;
@@ -45,15 +70,41 @@ btnAjouter.addEventListener('click', () => {
     /* Ajouter un événement pour supprimer l'element newDiv */
     btnSupprimer.addEventListener('click', () => {
         btnSupprimer.parentElement.remove();
+        let nbEnfants = listTache.childNodes.length;
+        if ( nbEnfants < 3 ) {
+            textInfos.textContent = `Vous avez ${nbEnfants - 1} tâche en cours`;
+            textInfos.classList.remove('textInfosError')
+        } else {textInfos.textContent = `Vous avez ${nbEnfants - 1} tâches en cours`
+        textInfos.classList.remove('textInfosError')
+    };
+        
     });
 
     /* Ajouter un evenemenet sur toutSupprBtn  */
     toutSupprBtn.forEach(btn => {
         btn.addEventListener('click', () => {
             listTache.removeChild(newDiv);
+            let nbEnfants = listTache.childNodes.length;
+            if ( nbEnfants < 3 ) {
+                textInfos.textContent = `Vous avez ${nbEnfants - 1} tâche en cours`;
+                textInfos.classList.remove('textInfosError')
+            } else {textInfos.textContent = `Vous avez ${nbEnfants - 1} tâches en cours`
+            textInfos.classList.remove('textInfosError')
+        };
         });
     });
+};
 
+btnAjouter.addEventListener('click', () => {
+    evenement();
+});
+
+const input = document.querySelector('#tache');
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        evenement();
+    };
 });
 
 
@@ -69,6 +120,6 @@ btnAide.addEventListener('click', () => {
 
 btnFerm.addEventListener('click', () => {
     aideModal.style.display = 'none';
-})
+});
 
 });
